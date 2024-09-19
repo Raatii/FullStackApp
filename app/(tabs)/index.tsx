265d76f1f6967dd005c1972b7381b128ac61cc6b
Rati <ratisharma94289@gmail.com>
2024-09-19 13:45:56 +0530

    FullStackApp

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,20 +7,20 @@ import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
+     <ParallaxScrollView
+   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+    headerImage={
+          <Image
+  source={require('@/assets/images/png-development-blue.png')}
+         style={styles.reactLogo}
+    />
+     }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome Rati!</ThemedText>
         <HelloWave />
-      </ThemedView>
+      </ThemedView> 
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Hi</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
@@ -45,11 +45,10 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
-      </ThemedView>
+      </ThemedView> 
     </ParallaxScrollView>
-  );
+  )
 }
-
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
